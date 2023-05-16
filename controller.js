0d9fb3e2f1b32a9c726eae2f059c4fb918520580
cumloverr <heribertusheri121@gmail.com>
2023-05-16 08:12:53 +0700

    menampilkan data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("aplikasi REST API berjalan !", res)
 };
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req, res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows ,fileds){
+        if(error){
+            connection.log(eror);
+        }else{
+            response.ok(rows, res)
+        }
+    });
+};
